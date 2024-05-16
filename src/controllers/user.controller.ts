caf3a31eb273aec2e0afb680765ca36087ecbd11
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2024-05-15 21:34:58 -0300

    rotas de usuário definidas

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,15 @@ import User from "../entity/user.entity";
 import { AppDataSource } from "../data-source";
 import { Request, Response, NextFunction } from "express";
 
+const createUser = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    await AppDataSource.getRepository(User).save(request.body);
+    response.status(201).send(request.body);
+  } catch (error) {
+    next(error);
+  }
+}
+
 const getUsers = async (_request: Request, response: Response, next: NextFunction) => {
   try {
     await AppDataSource.getRepository(User).find().then(users => {
@@ -12,6 +21,49 @@ const getUsers = async (_request: Request, response: Response, next: NextFunctio
   }
 }
 
+const getUser = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    await AppDataSource.getRepository(User).findOneBy({
+      id: request.params.id
+    })
+      .then(user => {
+        response.status(200).send(user);
+      });
+  } catch (error) {
+    next(error);
+  }
+}
+
+const updateUser = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const user = await AppDataSource.getRepository(User).findOneBy({
+      id: request.params.id,
+    });
+    if (user) {
+      AppDataSource.getRepository(User).merge(user, request.body);
+      const results = await AppDataSource.getRepository(User).save(user);
+      return response.send(results);
+    } else {
+      return response.status(404).send("User not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+const deleteUser = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const result = await AppDataSource.getRepository(User).delete(request.params.id)
+    return response.send(result)
+  } catch (error) {
+    next(error);
+  }
+}
+
 export default {
-  getUsers
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,5 +4,11 @@ import UserController from "../controllers/user.controller";
 const router = express.Router();
 
 router.get("/users", UserController.getUsers);
+router.post("/users", UserController.createUser)
+
+router.get("/users/:id", UserController.getUser);
+router.put("/users/:id", UserController.updateUser)
+
+router.delete("/users/:id", UserController.deleteUser)
 
 export default router;
