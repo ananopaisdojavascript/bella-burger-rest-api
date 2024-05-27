a2e89d9fbf236dcd0f49603aa5a346bbfd77b257
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2024-05-27 20:42:47 -0300

    imagem docker

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,14 @@ import AppDataSource from './database/data-source';
 import app, { logger } from './app';
 dotenv.config();
 
-AppDataSource;
+const port = process.env.PORT || 3000;
 
-  const port = process.env.PORT || 3000;
-
-  app.listen(port, () => {
-    try {
-      logger.info(`Bella Burger API is running on port ${port}`);
-    } catch (error) {
-      logger.error(error)
-    }
-  });
+app.listen(port, async () => {
+  try {
+    await AppDataSource.initialize()
+    logger.info(`Bella Burger API is running on port ${port}`);
+  } catch (error) {
+    logger.error(error)
+  }
+});
 
